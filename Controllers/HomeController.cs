87b8ec39bf2932df615fefcc0edd4a247005e037
200359764 <31934766+200359764@users.noreply.github.com>
2018-10-08 18:05:04 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace High_M_Music_Records.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About High M Music Records";
+
+            return View("About-Us");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Artists()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/RecordsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace High_M_Music_Records.Controllers
+{
+    public class RecordsController : Controller
+    {
+        // GET: Records
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Artist(String ArtistName)
+        {
+            ViewBag.ArtistName = ArtistName;
+            return View();
+        }
+    }
+}
